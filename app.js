8f5a4c05285c3125915729c66a1e100959d5e8ec
Kuzmich87 <d.kuzmich87@gmail.com>
2023-02-23 16:01:28 +0200

    fh

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
- 
 require("dotenv").config();
 const contactsRouter = require("./routes/api/contacts");
+const usersRouter = require("./routes/api/users");
+const currentRouter = require("./routes/api/current");
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
@@ -21,7 +24,7 @@ app.use((req, res) => {
 
 app.use((err, req, res, next) => {
   const { status = 500 } = err;
-  res.status(500).json({ message: err.message });
+  res.status(status).json({ message: err.message });
 });
 
 module.exports = app;
